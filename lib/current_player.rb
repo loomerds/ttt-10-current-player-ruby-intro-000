489e81b642346355868393ad202be8c74b15c78f
Doug Loomer <loomerds@gmail.com>
2019-05-24 13:02:33 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -1,7 +1,7 @@
 def turn_count(board)
   count turns = 0
   board.each do |turn|
-    if turn == "X" || turn == "O"
+    if turn == "X" || turn == "O" || turn == "x" || turn == "o"
       turns += 1
       puts turns
     end
