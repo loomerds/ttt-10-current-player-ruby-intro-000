53e85aba0709db41aa6dc868751f339948c3aa87
Doug Loomer <loomerds@gmail.com>
2019-05-24 13:17:39 +0000

    Automatically backed up by Learn

--- lib/current_player.rb
+++ lib/current_player.rb
@@ -9,10 +9,10 @@ def turn_count(board)
   return turns
 end
 
-def current_player(turns)
-  if turns % 2 == 0
-    return "O"
-  else
+def current_player(board)
+  if turnCount(board)  % 2 == 0
     return "X"
+  else
+    return "O"
   end
 end
